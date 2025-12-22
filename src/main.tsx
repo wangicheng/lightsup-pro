c38bf677dd3cfdd0b3924d6067b517b8a2256ca5
h_bugw7 <30070315+wangicheng@users.noreply.github.com>
2025-12-22 17:12:01 +0800

    Refactor code structure for improved readability and maintainability

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// 引入圖片 (這樣 Vite 才會把它轉成 Base64)
+import favicon from './assets/lightsup.png'
+
+// 動態將 favicon 寫入 head
+const link = document.createElement('link');
+link.rel = 'icon';
+link.href = favicon; // 這裡的 favicon 已經是 data:image/png;base64,....
+document.head.appendChild(link);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
